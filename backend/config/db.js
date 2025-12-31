d835314ee3f4a559cad2156f7b9bbdedfdf4486d
Musab Hassen <mushas1248@gmail.com>
2026-01-01 00:14:50 +0300

    fixed db config

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,8 +8,6 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGODB_URI, {
       serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
       socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-      bufferMaxEntries: 0, // Disable mongoose buffering
-      bufferCommands: false, // Disable mongoose buffering
     });
 
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
